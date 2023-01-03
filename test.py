d88f9ef012c8649aa6866c1ca5e62dcb3deffe3c
ChethanG7 <60126799+ChethanG7@users.noreply.github.com>
2023-01-03 15:19:47 +0530

    Update test.py

--- test.py
+++ test.py
@@ -33,38 +33,25 @@ def main():
             summary_choice = st.selectbox("Summary Choice" , ["Gensim","Sumy Lex rank","NLTK"])
             if st.button("Summarize Via Text"):
                 if summary_choice == 'Gensim':
-                    summary_result = summarize(raw_text)
-
+                    try:
+                        summary_result = summarize(raw_text)
+                    except:
+                        summary_result = raw_text
                 elif summary_choice == 'Sumy Lex rank':
                     summary_result = summarize(raw_text)
 
                 elif summary_choice == 'NLTK':
                     summary_result = summarize(raw_text)
-
-
+                    
                 st.write(summary_result)
 
         if choice == "Summazrize via File":
             st.write("""
             ### Document Text Summary
             """)
-            
-            def process(data,column_choice,summary_choice):
-#                 def convert_df(data):
-#                     output = BytesIO()
-#                     writer = pd.ExcelWriter(output, engine='xlsxwriter')
-#                     data.to_excel(writer, index=False, sheet_name='Sheet1')
-#                     processed_data = output.getvalue()
-#                     return processed_data
-#                       return data
-                return data
-#                 st.download_button("Press to Download",
-#                                    convert_df(data),
-#                                    "processed_file.xlsx")
-            
+               
             uploaded_file = st.file_uploader("Choose a Excel file")
             if uploaded_file is not None:
-        #         bytes_data = uploaded_file.getvalue()
                 with st.spinner('Wait for it...'):    
                     data = pd.read_excel(uploaded_file)     
                     st.write('Preview of the attached file',data.head(10))
@@ -79,7 +66,10 @@ def main():
                     if submit_data:                                       
                         with col4:
                             with st.spinner('Processing...'):
-                                time.sleep(5)
+                                def get_summary(data,column_choice,summary_choice):
+                                    
+                                    return data                              
+                                
                                 def to_excel(df):
                                     output = BytesIO()
                                     writer = pd.ExcelWriter(output, engine='xlsxwriter')
@@ -91,33 +81,14 @@ def main():
                                     writer.save()
                                     processed_data = output.getvalue()
                                     return processed_data
+                                
+                                data = get_summary(data,column_choice,summary_choice)
                                 df_xlsx = to_excel(data)
-                                st.download_button(label='📥 Download Current Result',
+                                st.download_button(label='📥 Download Processed Result',
                                                                 data=df_xlsx ,
                                                                 file_name= 'df_test.xlsx')
 #                                 st.button("Download Processed file")
 #                             st.text("Processed Successfully")
-         
-        
-            
-    #         st.session_state["preview"] = data[:10]
-    # #         for i in range(0, min(5, len(data))):
-    # #             st.session_state["preview"] = st.session_state["preview"]+ data[i]
-    #     preview = st.text_area("File Preview", "", height=150, key="preview")
-    #     upload_state = st.text_area("Upload State", "", key="upload_state")
-    #     def upload():
-    #         if uploaded_file is None:
-    #             st.session_state["upload_state"] = "Upload a file first!"
-    #         else:
-    #             data = uploaded_file.getvalue()
-    #             parent_path = pathlib.Path(__file__).parent.parent.resolve()           
-    #             save_path = os.path.join(parent_path, "data")
-    #             complete_name = os.path.join(save_path, uploaded_file.name)
-    #             destination_file = open(complete_name, "w")
-    #             destination_file.write(data)
-    #             destination_file.close()
-    #             st.session_state["upload_state"] = "Saved " + complete_name + " successfully!"
-    #     st.button("Upload file to Sandbox", on_click=upload)
     
 if __name__ == '__main__':
     main()
